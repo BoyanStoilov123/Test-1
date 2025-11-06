890dc8e08469604588fe32f446aed1b4353fc862
BoyanStoilov123 <et.boyan.s@eduiteh.eu>
2025-11-06 10:26:52 +0200

    Zad 1 test 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Type a number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Type a digit: ");
+int digit = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+if (number < 0)
+{
+    number = -number;
+}
+while (number > 0)
+{
+    int lastDigit = number % 10;
+    if (lastDigit == digit)
+    {
+        count++;
+    }
+    number = number / 10; 
+}
+
+Console.WriteLine(count);
+
