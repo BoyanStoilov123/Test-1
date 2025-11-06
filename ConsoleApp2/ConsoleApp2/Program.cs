3bf2960fd24d880460fc634b510652f2d12d67fc
BoyanStoilov123 <et.boyan.s@eduiteh.eu>
2025-11-06 10:41:44 +0200

    Zad 2 test 1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Type a positive number");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < n; i ++)
+{
+    if (n % i == 0)
+    {
+        if (i == n / 2 || n % (i + 1) != 0 && i > n / 2)
+            Console.Write(i);
+        else
+            Console.Write(i + ", ");
+    }
+}   
